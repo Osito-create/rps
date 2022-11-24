4fc13339269fa917b202c5c0b2eec5d327acf323
Osito <93452519+Osito-create@users.noreply.github.com>
2022-11-24 21:40:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"use strict";
+// random return Rock, Paper or Scissors
+
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
+}
+
+console.log(getRandomChoice);
